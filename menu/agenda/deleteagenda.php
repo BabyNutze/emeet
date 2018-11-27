8cb181d4559f61bc26cd39e3d48d7083f3c4b407
BabyNutze <babysparker@live.com>
2018-11-27 09:35:01 +0700

    Create deleteagenda.php

---
+++ menu/agenda/deleteagenda.php
@@ -0,0 +1,34 @@
+<?php
+// Process delete operation after confirmation
+if ( isset( $_GET[ "ag" ] ) && !empty( $_GET[ "ag" ] ) ) {
+	// Include config file
+
+	// Prepare a delete statement
+	$sql = "DELETE FROM term WHERE agenda_id = " . $_GET[ 'ag' ] ;
+	if ( mysqli_query( $conn, $sql ) ) {
+
+		echo "ลบวาระแล้ว\n";
+
+		$sql = "DELETE FROM agenda WHERE agenda_id = " .$_GET['ag'] ;
+		if ( mysqli_query( $conn, $sql ) ) {
+			echo "ลบการประชุมแล้ว\n";
+					//echo "<script>window.location='home.php?menu=agenda'</script>";
+			echo "<script>setTimeout(function() {  window.location.href = 'home.php?menu=agenda';}, 1000);</script>";
+		} else {
+			echo "Error deleting record: " . mysqli_error( $conn );
+		}
+
+	} else {
+		echo "Error deleting record: " . mysqli_error( $conn );
+	}
+
+
+} else {
+	// Check existence of id parameter
+	if ( empty( trim( $_GET[ "ag" ] ) ) ) {
+		// URL doesn't contain id parameter. Redirect to error page
+		header( "location: home.php?menu=agenda" );
+		exit();
+	}
+}
+?>
