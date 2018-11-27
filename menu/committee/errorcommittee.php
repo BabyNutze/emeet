003e421de3349bf2805fe5c18367eec6f2fc1c81
BabyNutze <babysparker@live.com>
2018-11-27 09:34:29 +0700

    Create errorcommittee.php

---
+++ menu/committee/errorcommittee.php
@@ -0,0 +1,16 @@
+    <div class="wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="page-header">
+                        <h1>เกิดข้อผิดพลาด</h1>
+                    </div>
+                    <div>
+                        <p><a href="home.php?menu=committee" class="alert-link">ลองใหม่อีกครั้ง</p>
+                    </div>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
