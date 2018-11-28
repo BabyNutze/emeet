7c6169b7c4722deb90981d597b3eee7a97bfc9d7
BabyNutze <babysparker@live.com>
2018-11-28 08:09:14 +0700

    Update addsubterm3.php

--- menu/agenda/addsubterm3.php
+++ menu/agenda/addsubterm3.php
@@ -1,7 +1,7 @@
 <?php
 
 
-if ( isset( $_GET[ "ag" ] ) && !empty( trim( $_GET[ "ag" ] && isset( $_GET[ "t" ] ) && !empty( trim( $_GET[ "t" ] ) ) ) {
+if ( isset( $_GET[ "ag" ] ) && !empty( trim( $_GET[ "ag" ] && isset( $_GET[ "t" ] ) && !empty( trim( $_GET[ "t" ] ) ) ) ) ) {
 
 	// Prepare a select statement
 	$sql = "SELECT agenda.agenda_id, agenda.agenda_subject,  DATE_FORMAT(meeting_day,'%d/%m/%Y') as md,TIME_FORMAT(start_time, '%H:%i') as st,	TIME_FORMAT(end_time, '%H:%i') as et , term_no, term_subject , subterm.subterm_subject
@@ -36,40 +36,34 @@ if ( isset( $_GET[ "ag" ] ) && !empty( trim( $_GET[ "ag" ] && isset( $_GET[ "t" 
 	}
 }
 
-if ( isset( $_POST[ "agenda_id" ] ) && !empty( $_POST[ 'hdnCount' ] ) && isset( $_POST[ "term_id" ] ) ) {
-	for ( $i = 1; $i <= ( int )$_POST[ "hdnCount" ]; $i++ ) {
-		$sql = "SELECT MAX(subterm_id) AS subterm_id FROM subterm";
-		$result = mysqli_query( $conn, $sql );
-		if ( $result->num_rows > 0 ) {
-			// output data of each row
-			if ( $row = $result->fetch_assoc() ) {
-				$subterm_id = $row[ "subterm_id" ] + 1;
-			}
-		} else {
-			$subterm_id = 1;
+if ( isset( $_POST[ "agenda_id" ] ) && !empty( $_POST[ 'subterm_subject' ] ) && isset( $_POST[ "term_id" ] ) ) {
+	$sql = "SELECT MAX(subterm_id) AS subterm_id FROM subterm";
+	$result = mysqli_query( $conn, $sql );
+	if ( $result->num_rows > 0 ) {
+		// output data of each row
+		if ( $row = $result->fetch_assoc() ) {
+			$subterm_id = $row[ "subterm_id" ] + 1;
 		}
-		$term_id = $_POST[ "term_id" ];
-
-
-		if ( isset( $_POST[ "subterm_no$i" ] ) ) {
-			if ( $_POST[ "subterm_no$i" ] != "" ) {
-
-				$sql = "INSERT INTO subterm (subterm_id,  agenda_id, term_id, subterm_no, subterm_subject) 
-					VALUES ($subterm_id, $agenda_id, $term_id, '" . $_POST[ "subterm_no$i" ] . "','" . $_POST[ "subterm_subject$i" ] . "')";
-				echo $sql;
-				$query = mysqli_query( $conn, $sql );
-				if ( $conn->query( $sql ) === TRUE ) {
-					echo "New record created successfully";
-				} else {
-					echo "Error: " . $sql . "<br>" . $conn->error;
-				}
-
-
-
-
-			}
-		}
+	} else {
+		$subterm_id = 1;
 	}
+	$term_id = $_POST[ "term_id" ];
+	$subterm_subject = $_POST[ 'subterm_subject' ];
+	$subterm_detail = $_POST[ 'subterm_detail' ];
+	$agenda_id = $_POST[ 'agenda_id' ];
+
+
+	$sql = "INSERT INTO subterm (subterm_id,  agenda_id, term_id, subterm_subject, subterm_detail) 
+					VALUES ($subterm_id, $agenda_id, $term_id, '$subterm_subject', '$subterm_detail' )";
+
+	$query = mysqli_query( $conn, $sql );
+	if ( $conn->query( $sql ) === TRUE ) {
+		echo "บันทึกข้อมูลแล้ว";
+		echo "<script>setTimeout(function() {  window.location.href = 'home.php?menu=agenda&sub=termdetail&ag=$agenda_id&t=$term_id';}, 1000);</script>";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+
 
 } else {
 
@@ -88,9 +82,11 @@ if ( isset( $_POST[ "agenda_id" ] ) && !empty( $_POST[ 'hdnCount' ] ) && isset( 
 							</li>
 							<li class="breadcrumb-item" aria-current="page"><a href="home.php?menu=agenda">งานประชุม</a>
 							</li>
-							<li class="breadcrumb-item"><a href="home.php?menu=agenda&sub=read&ag=<?php echo $agenda_id;?>">
-								<?php echo $agenda_subject . " วันที่ " . $md. " เวลา " . $st . "-" . $et ; ?></a>
-							</li>							
+							<li class="breadcrumb-item">
+								<a href="home.php?menu=agenda&sub=read&ag=<?php echo $agenda_id;?>">
+									<?php echo $agenda_subject . " วันที่ " . $md. " เวลา " . $st . "-" . $et ; ?>
+								</a>
+							</li>
 							<li class="breadcrumb-item active" aria-current="page">
 								<?php echo $term_subject ; ?>
 							</li>
@@ -104,52 +100,41 @@ if ( isset( $_POST[ "agenda_id" ] ) && !empty( $_POST[ 'hdnCount' ] ) && isset( 
 				<br>
 				<h3>
 					<?php echo $agenda_subject ;  ?>
-				</h3>
-				<form action="" id="addsubtermform" name="addtermform" method="post">
-					<h5>
+				</h3>					<h5>
 						<?php echo $term_no ." " .$term_subject ;  ?>
 					</h5>
-					<br>
-					<label for="editor1">รายละเอียด</label>
-					<textarea id="editor1">
-		&lt;h2 style="text-align: center;"&gt;The Flavorful Tuscany Meetup&lt;/h2&gt;
-		&lt;p style="text-align: center;"&gt;&lt;span style="color: #007ac9;"&gt;&lt;strong&gt;Welcome letter&lt;/strong&gt;&lt;/span&gt;&lt;/p&gt;
-		&lt;p&gt;Dear Guest,&lt;/p&gt;
-		&lt;p&gt;We are delighted to welcome you to the annual &lt;em&gt;Flavorful Tuscany Meetup&lt;/em&gt; and hope you will enjoy the programme as well as your stay at the Bilancino Hotel.&lt;/p&gt;
-		&lt;p&gt;Please find below the full schedule of the event.&lt;/p&gt;
-		&lt;table class="schedule" cellpadding="15" cellspacing="0" style="border-collapse:collapse;width:100%;"&gt;
-			&lt;thead&gt;
-				&lt;tr&gt;
-					&lt;th colspan="2" scope="col" style="background-color: #F2F9FF; text-align: center; font-size: 21px;"&gt;&lt;span&gt;Saturday, July 14&lt;/span&gt;&lt;/th&gt;
-				&lt;/tr&gt;
-			&lt;/thead&gt;
-			&lt;tbody&gt;
-				&lt;tr&gt;
-					&lt;td style="white-space:nowrap;"&gt;&lt;span&gt;9:30 AM - 11:30 AM&lt;/span&gt;&lt;/td&gt;
-					&lt;td&gt;&lt;span&gt;Americano vs. Brewed - “know your coffee” session with &lt;strong&gt;Stefano Garau&lt;/strong&gt;&lt;/span&gt;&lt;/td&gt;
-				&lt;/tr&gt;
-				&lt;tr&gt;
-					&lt;td style="white-space:nowrap;"&gt;&lt;span&gt;1:00 PM - 3:00 PM&lt;/span&gt;&lt;/td&gt;
-					&lt;td&gt;&lt;span&gt;Pappardelle al pomodoro - live cooking session with &lt;strong&gt;Rita Fresco&lt;/strong&gt;&lt;/span&gt;&lt;/td&gt;
-				&lt;/tr&gt;
-				&lt;tr&gt;
-					&lt;td style="white-space:nowrap;"&gt;&lt;span&gt;5:00 PM - 8:00 PM&lt;/span&gt;&lt;/td&gt;
-					&lt;td&gt;&lt;span&gt;Tuscan vineyards at a glance - wine-tasting session with &lt;strong&gt;Frederico Riscoli&lt;/strong&gt;&lt;/span&gt;&lt;/td&gt;
-				&lt;/tr&gt;
-			&lt;/tbody&gt;
-		&lt;/table&gt;
-		&lt;blockquote&gt;
-			&lt;p&gt;The annual Flavorful Tuscany meetups are always a culinary discovery. You get the best of Tuscan flavors during an intense one-day stay at one of the top hotels of the region. All the sessions are lead by top chefs passionate about their profession. I would certainly recommend to save the date in your calendar for this one!&lt;/p&gt;
-			&lt;p&gt;Angelina Calvino, food journalist&lt;/p&gt;
-		&lt;/blockquote&gt;
-		&lt;p&gt;Please arrive at the Bilancino Hotel reception desk at least &lt;strong&gt;half an hour earlier&lt;/strong&gt; to make sure that the registration process goes as smoothly as possible.&lt;/p&gt;
-		&lt;p&gt;We look forward to welcoming you to the event.&lt;/p&gt;
-		&lt;p&gt;&lt;/p&gt;
-		&lt;p&gt;&lt;strong&gt;Victoria Valc&lt;/strong&gt;&lt;/p&gt;
-		&lt;p&gt;&lt;strong&gt;Event Manager&lt;/strong&gt;&lt;/p&gt;
-		&lt;p&gt;&lt;strong&gt;Bilancino Hotel&lt;/strong&gt;&lt;/p&gt;
-	</textarea>
+				<?php 
+				$sql = "SELECT * FROM subterm where term_id = $term_id and agenda_id = $agenda_id";
+	$result = mysqli_query( $conn, $sql );
+	if ( $result->num_rows > 0 ) {
+		// output data of each row
+		if ( $row = $result->fetch_assoc() ) {
+			echo $row['subterm_subject']. "<br>";
+		}
+	} else {
+echo "ยังไม่มีวาระย่อย";
+	}
+
 				
+				
+				
+				?>
+				<form action="" id="addsubtermform" name="addtermform" method="post">
+
+					<br>
+
+					<div class="form-group">
+						<label for="email">ชื่อวาระย่อย:</label>
+						<input type="text" name="subterm_subject" class="form-control">
+					</div>
+					<div class="form-group">
+						<label for="subterm_detail">รายละเอียด</label><br>
+						<textarea id="subterm_detail" name="subterm_detail" class="form-control"></textarea>
+
+
+					</div>
+					<input type="hidden" name="agenda_id" value="<?php echo $agenda_id;?> ">
+					<input type="hidden" name="term_id" value="<?php echo $term_id;?> ">
 
 
 					<input type="submit" value="บันทึก">
@@ -161,37 +146,12 @@ if ( isset( $_POST[ "agenda_id" ] ) && !empty( $_POST[ 'hdnCount' ] ) && isset( 
 	</div>
 </div>
 <script>
-	CKEDITOR.plugins.add( 'uploadfile', {
-		requires: 'uploadwidget,link',
-		init: function( editor ) {
-			// Do not execute this paste listener if it will not be possible to upload file.
-			if ( !CKEDITOR.plugins.clipboard.isFileApiSupported ) {
-				return;
-			}
+	var editor = CKEDITOR.replace( 'subterm_detail', {
 
-			var fileTools = CKEDITOR.fileTools,
-				uploadUrl = fileTools.getUploadUrl( editor.config );
+	} );
 
-			if ( !uploadUrl ) {
-				CKEDITOR.error( 'uploadfile-config' );
-				return;
-			}
-
-			fileTools.addUploadWidget( editor, 'uploadfile', {
-				uploadUrl: fileTools.getUploadUrl( editor.config ),
-
-				fileToElement: function( file ) {
-					// Show a placeholder with an empty link during the upload.
-					var a = new CKEDITOR.dom.element( 'a' );
-					a.setText( file.name );
-					a.setAttribute( 'href', '#' );
-					return a;
-				},
-
-				onUploaded: function( upload ) {
-					this.replaceWith( '<a href="' + upload.url + '" target="_blank">' + upload.fileName + '</a>' );
-				}
-			} );
-		}
+	editor.on( 'change', function ( evt ) {
+		// getData() returns CKEditor's HTML content.
+		console.log( 'Total bytes: ' + evt.editor.getData().length );
 	} );
 </script>
