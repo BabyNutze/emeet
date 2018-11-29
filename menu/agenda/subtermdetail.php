8aab0dd5dfa7de7ab176c8b09ad9ed82ae322579
BabyNutze <babysparker@live.com>
2018-11-29 12:24:51 +0700

    Update subtermdetail.php

--- menu/agenda/subtermdetail.php
+++ menu/agenda/subtermdetail.php
@@ -5,7 +5,7 @@ if ( isset( $_GET[ "a" ] ) && isset( $_GET[ "t" ] )  && isset( $_GET[ "s" ] ) ) 
 
 	$sql = "SELECT agenda.agenda_id, agenda.agenda_subject,  DATE_FORMAT(meeting_day,'%d/%m/%Y') as md,
 TIME_FORMAT(start_time, '%H:%i') as st,	TIME_FORMAT(end_time, '%H:%i') as et , 
-term.term_no, term.term_subject , subterm.subterm_subject, term.term_detail, subterm.subterm_detail
+term.term_no, term.term_subject , subterm.subterm_subject, subterm.subterm_no, term.term_detail, subterm.subterm_detail, subterm.subterm_resolution 
 	FROM agenda 
 	LEFT JOIN term ON agenda.agenda_id = term.agenda_id 
 	LEFT JOIN subterm on subterm.term_id = term.term_id and agenda.agenda_id = subterm.agenda_id
@@ -26,13 +26,16 @@ term.term_no, term.term_subject , subterm.subterm_subject, term.term_detail, sub
 			$term_no = $row[ "term_no" ];
 			$term_detail = $row[ "term_detail" ];
 			$subterm_subject = $row[ "subterm_subject" ];
+			$subterm_no = $row[ "subterm_no" ];
 			$subterm_detail = $row[ "subterm_detail" ];
+			$subterm_resolution = $row["subterm_resolution"];
+			
 			
 			
 
 		} else {
-			// URL doesn't contain valid id parameter. Redirect to error page
-			header( "location: error.php" );
+
+
 			exit();
 		}
 
@@ -40,6 +43,35 @@ term.term_no, term.term_subject , subterm.subterm_subject, term.term_detail, sub
 		echo "Oops! Something went wrong. Please try again later.";
 	}
 }
+
+if ( isset( $_POST[ 'btnres' ] ) ) {
+
+	if ( !empty( $_POST[ 'term_resolution' ] ) && !empty( $_POST[ 'agenda_id' ] ) && !empty( $_POST[ 'term_id' ] ) && !empty( $_POST[ 'subterm_id' ] ) ) {
+		$agenda_id = $_POST[ 'agenda_id' ];
+		$term_id = $_POST[ 'term_id' ];
+		$subterm_id = $_POST[ 'subterm_id' ];
+		$subterm_resolution = $_POST[ 'term_resolution' ];
+
+		$sql = "UPDATE term SET term_resolution ='$term_resolution' WHERE agenda_id = $agenda_id and term_id= $term_id";
+		if ( mysqli_query( $conn, $sql ) ) {
+
+		} else {
+
+			echo "<script>setTimeout(function() {  window.location.href = 'home.php?menu=agenda&sub=termdetail&a=$agenda_id&t=$term_id';}, 1000);</script>";
+		}
+
+
+
+
+
+
+
+	} else {
+		echo "<script type='text/javascript'>alert('กรุณากรอกมติการประชุมด้วย');</script>";
+	}
+
+}
+
 
 ?>
 
@@ -55,8 +87,8 @@ term.term_no, term.term_subject , subterm.subterm_subject, term.term_detail, sub
 							<li class="breadcrumb-item"><a href="home.php?menu=agenda">งานประชุม</a>
 							</li>
 							<li class="breadcrumb-item">
-								<a href="home.php?menu=agenda&sub=read&ag=<?php echo $agenda_id;?>">
-									<?php echo $agenda_subject . " วันที่ " . $md. " เวลา " . $st . "-" . $et ; ?>
+								<a href="home.php?menu=agenda&sub=read&a=<?php echo $agenda_id;?>">
+									<?php echo $agenda_subject ; ?>
 								</a>
 							</li>
 							<li class="breadcrumb-item active" aria-current="page">
@@ -80,7 +112,7 @@ term.term_no, term.term_subject , subterm.subterm_subject, term.term_detail, sub
 					</h5>
 			
 
-				<div class="float-right"><a href='home.php?menu=agenda&sub=edittermdetail&ag=<?php echo $agenda_id;?>&t=<?php echo $term_id;?>' title='แก้ไข' data-toggle='tooltip'><span><i class='fas fa-edit fa-2x'></i></span></a>
+				<div class="float-right"><a href='home.php?menu=agenda&sub=edittermdetail&a=<?php echo $agenda_id;?>&t=<?php echo $term_id;?>' title='แก้ไข' data-toggle='tooltip'><span><i class='fas fa-edit fa-2x'></i></span></a>
 				</div>
 				<br>
 				<input type="hidden" name="agenda_id" value="<?php echo $agenda_id;?>">
@@ -94,13 +126,28 @@ term.term_no, term.term_subject , subterm.subterm_subject, term.term_detail, sub
 				<div class="float-right">
 
 				</div>
-				<form action="home.php?menu=agenda&sub=savetermdetail" id="addsubtermform" name="addtermform" method="post">
+				<form action="" id="addtermform" name="addtermform" method="post" >
 					<h5><b><u>มติที่ประชุม</u></b></h5>
-					<div class="float-right"><a href='home.php?menu=agenda&sub=edittermdetail&ag=<?php echo $agenda_id;?>&t=<?php echo $term_id;?>' title='แก้ไข' data-toggle='tooltip'><span><i class='fas fa-edit fa-2x'></i></span></a>
+					<div class="float-right"><a href='home.php?menu=agenda&sub=edittermdetail&a=<?php echo $agenda_id;?>&t=<?php echo $term_id;?>' title='แก้ไข' data-toggle='tooltip'><span><i class='fas fa-edit fa-2x'></i></span></a>
 					</div>
-					<textarea rows="3" class="form-control"></textarea>
-					<button type="submit" >บันทึกมติ</button>
+					<?php
+
+					if ( !empty( $subterm_resolution ) ) {
+
+						echo "<input type='text' name=subterm_resolution' class='form-control' value='$subterm_resolution' ></input> ";
+					} else {
+						echo "<input type='text' name='subterm_resolution' class='form-control' ></input> ";
+
+					}
+					?>
+
+					<input type="hidden" name="agenda_id" id="agenda_id" value="<?php echo $agenda_id; ?>">
+					<input type="hidden" name="term_id" value="<?php echo $term_id; ?>">
+
+					<button type="submit" name="btnres" class="btn btn-primary">บันทึก</button>
+
 				</form>
+
 
 
 			</div>
