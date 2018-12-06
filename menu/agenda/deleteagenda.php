bffbcd620f272701daff33cb16cb76520b340328
BabyNutze <babysparker@live.com>
2018-12-06 09:36:58 +0700

    Update deleteagenda.php

--- menu/agenda/deleteagenda.php
+++ menu/agenda/deleteagenda.php
@@ -1,15 +1,15 @@
 <?php
 // Process delete operation after confirmation
-if ( isset( $_GET[ "ag" ] ) && !empty( $_GET[ "ag" ] ) ) {
+if ( isset( $_GET[ "a" ] ) && !empty( $_GET[ "a" ] ) ) {
 	// Include config file
 
 	// Prepare a delete statement
-	$sql = "DELETE FROM term WHERE agenda_id = " . $_GET[ 'ag' ] ;
+	$sql = "DELETE FROM term WHERE agenda_id = " . $_GET[ 'a' ] ;
 	if ( mysqli_query( $conn, $sql ) ) {
 
 		echo "ลบวาระแล้ว\n";
 
-		$sql = "DELETE FROM agenda WHERE agenda_id = " .$_GET['ag'] ;
+		$sql = "DELETE FROM agenda WHERE agenda_id = " .$_GET['a'] ;
 		if ( mysqli_query( $conn, $sql ) ) {
 			echo "ลบการประชุมแล้ว\n";
 					//echo "<script>window.location='home.php?menu=agenda'</script>";
@@ -25,7 +25,7 @@ if ( isset( $_GET[ "ag" ] ) && !empty( $_GET[ "ag" ] ) ) {
 
 } else {
 	// Check existence of id parameter
-	if ( empty( trim( $_GET[ "ag" ] ) ) ) {
+	if ( empty( trim( $_GET[ "a" ] ) ) ) {
 		// URL doesn't contain id parameter. Redirect to error page
 		header( "location: home.php?menu=agenda" );
 		exit();
